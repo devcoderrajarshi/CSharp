751e46b0a23e2a91a8846f9c48e6bcf8ff3fedd4
devcoderrajarshi <rajarshiisagoodboy@gmail.com>
2018-03-03 22:29:53 +0530

    added string pasting application

---
+++ string_pasting(Using C#)/string_pasting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+namespace string_pasting
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"C:\Users\Rajarshi\Documents\test\demo.txt");
+
+            // Display the file contents by using a foreach loop.
+            Console.WriteLine("Contents of WriteLines2.txt = ");
+            foreach (string line in lines)
+            {
+                // Use a tab to indent each line of the file.
+
+
+
+
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
